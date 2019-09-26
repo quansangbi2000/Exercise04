9a85ecd7e2d67ba04228263a296e802f44d6eb93
quansangbi2000 <quansangbi20@gmail.com>
2019-09-26 17:44:21 +0700

    TestIsOdd

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("hello github");
+        }
+        public static bool IsOdd(int n)
+        {
+            return false;
         }
     }
 }
---
+++ ConsoleApplication1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIsOdd()
+        {
+            bool result03 = Program.IsOdd(3);
+            Assert.AreEqual(false, result03);
+
+            bool result04 = Program.IsOdd(4);
+            Assert.AreEqual(false, result04);
+        }
+    }
+}
